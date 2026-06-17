12bc7bce67bc36f8a00db2a72aa118234b783a4f
Mariaplk <machapalehova@gmail.com>
2026-06-17 16:24:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from config import Config
+from models import db
+
 
 app = Flask(__name__)
 
+app.config.from_object(Config)
+
+db.init_app(app)
+
+
 @app.route("/")
 def index():
-    return "Electronic Library"
+    return render_template(
+        "index.html",
+        books=[]
+    )
+
+
+@app.route("/login")
+def login():
+    return render_template(
+        "login.html"
+    )
+
 
 if __name__ == "__main__":
-    app.run()
+
+    with app.app_context():
+        db.create_all()
+
+    app.run(
+        debug=True
+    )
